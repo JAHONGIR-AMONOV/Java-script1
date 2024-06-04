153c22e2e0ee065622a877389d719212a73f2ffa
JAHONGIR-AMONOV <jahongir.amonov3010@gmail.com>
2024-06-04 12:45:43 +0500

    number

---
+++ script.js
@@ -0,0 +1,27 @@
+let number = prompt("Введите любое число:")  
+
+  if (number % 2 === 0) {
+    alert("Введенное число " + number + " - четное.");
+  } else {
+    alert("Введенное число " + number + " - нечетное.");
+  }
+
+  console.log(
+    number
+  );
+
+let question = prompt('Введите свое имя')
+
+if (question[0] ==='a'){
+    alert('Добро пожаловать!')
+} else {
+    alert('Извините вы неможете войти')
+}
+
+let answer = prompt('Введите свое имя')
+
+if (length % 2 === 0) {
+    alert('Welcome')
+} else {
+    alert('Sorry!')
+}
